d33d45f34af485b6a8bcacaf43de160a4083def6
dollarkillerx <adapawang@gmail.com>
2021-12-23 19:31:14 +0800

    init project

--- internel/config/config.go
+++ internel/config/config.go
@@ -1,4 +1,21 @@
 package config
 
 type Config struct {
+	Accounts []Account `json:"accounts"`
+}
+
+type Account struct {
+	Uid   string `json:"uid"`
+	Token string `json:"token"`
+}
+
+type Relation struct {
+	Announcer string `json:"announcer"` // 发布者
+	Follower  string `json:"follower"`  // 跟随者
+	Email     string `json:"email"`     // 通知邮件
+}
+
+type EmailServer struct {
+	SmtpServer string `json:"smtp_server"`
+	Port       int    `json:"port"`
 }
